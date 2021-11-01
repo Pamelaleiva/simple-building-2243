d7fe82c307b32f245a5b60aeee56701eeff54ec8
Pamelaleiva <pamelaleivag1@gmail.com>
2021-11-01 16:50:09 -0300

    agregando modulo de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  get "signup", to: "users#new", as: "signup"
+  get "login", to: "sessions#new", as: "login"
+  get "logout", to: "sessions#destroy", as: "logout"
   resources :apartments
   resources :buildings
-root "public#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
